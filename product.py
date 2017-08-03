6521bde06b9737c299d69414611bbaf0a07f554c
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-08-03 14:31:46 -0700

    product assignment

---
+++ product.py
@@ -0,0 +1,32 @@
+class Proucrt(object):
+	"""docstring for Sell"""
+	def __init__(self, price, item_name, weight, brand, cost, status):
+		self.price = price
+		self.item_name = item_name
+		self.weight = weight
+		self.brand = brand
+		self.cost = cost
+		self.status = default
+
+
+	def sell(self):
+
+			
+
+
+
+
+	def Add_tax(self):
+
+
+
+
+	def Return(self, reason):
+		self.reason = reason
+				
+
+
+								
+
+		
+		
