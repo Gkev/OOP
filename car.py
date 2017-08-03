fbffb47212089b6facc77222d9b2efe13305b579
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-08-03 11:57:46 -0700

    car assignment

---
+++ car.py
@@ -0,0 +1,31 @@
+class Car(object):
+	"""docstring for car"""
+	def __init__(self, price, speed, fuel, miles):
+		self.price = price
+		self.speed = speed
+		self.fuel = fuel
+		self.miles = miles
+		self.tax = 0
+
+	def display_all(self):
+			print 'Price: ' + str(self.price)
+			print 'Speed: ' + str(self.speed) + ' mph'
+			print 'Fuel: ' + str(self.fuel)
+			print 'Miles: ' + str(self.miles) + ' miles'
+
+			
+
+		
+		
+car3 = Car(30000, 20, 'not full', 50 )
+car4 = Car(4000, 220, 'empty', 50 )
+car5 = Car(15000, 140, 'full', 50 )
+car6 = Car(2000, 160, 'need gas', 50 )
+car7 = Car(9000, 70, 'empty', 50 )
+car8 = Car(13000, 120, 'not full', 50 )
+car3.display_all()
+car4.display_all()
+car5.display_all()
+car6.display_all()
+car7.display_all()
+car8.display_all()
